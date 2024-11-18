1ff3cae305bbeb288dbdaefea59efade0e4decfd
Duc Hoang <hoangduc201020@gmail.com>
2024-11-18 10:44:33 +0700

    listing course/LoginRegister

--- frontend/src/hooks/useRouterCustome.jsx
+++ frontend/src/hooks/useRouterCustome.jsx
@@ -1,13 +1,39 @@
-//import React from "react";
+// //import React from "react";
+
+// import { useRoutes } from "react-router-dom";
+// import HomeTemplate from "../Templates/HomeTemplate/HomeTemplate.jsx";
+// import { CourseSingle } from "../component/CourseSingle/CourseSingle.jsx";
+
+// const useRouterCustome = () => {
+//   const router = useRoutes([
+//     {
+//       path: "/",
+//       element: <HomeTemplate />,
+//       children: [
+//         {
+//           index: true,
+//           element: <CourseSingle />,
+//         },
+//       ],
+//     },
+//   ]);
+//   return router;
+// };
+
+// export default useRouterCustome;
+
+
 
 import { useRoutes } from "react-router-dom";
 import HomeTemplate from "../Templates/HomeTemplate/HomeTemplate.jsx";
 import { CourseSingle } from "../component/CourseSingle/CourseSingle.jsx";
+import CoursePage from "../component/CoursePage/CoursePage.jsx";
+import LoginRegister from "../component/LoginRegister/LoginRegister.jsx";
 
 const useRouterCustome = () => {
   const router = useRoutes([
     {
-      path: "/",
+      path: "/", 
       element: <HomeTemplate />,
       children: [
         {
@@ -16,8 +42,29 @@ const useRouterCustome = () => {
         },
       ],
     },
+    {
+      path: "/courses", 
+      element: <HomeTemplate />, 
+      children: [
+        {
+          index: true,
+          element: <CoursePage />,
+        },
+      ],
+    },
+    {
+      path: "/loginregister", 
+      element: <HomeTemplate />, 
+      children: [
+        {
+          index: true,
+          element: <LoginRegister />,
+        },
+      ],
+    },
   ]);
   return router;
 };
 
 export default useRouterCustome;
+
