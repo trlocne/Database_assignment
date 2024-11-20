328303a2e21b9fdbacbd4eb2b2d0ac29bff14971
Duc Hoang <hoangduc201020@gmail.com>
2024-11-20 16:46:07 +0700

    Update ViewMore

--- frontend/src/hooks/useRouterCustome.jsx
+++ frontend/src/hooks/useRouterCustome.jsx
@@ -1,70 +1,53 @@
 // //import React from "react";
 
-// import { useRoutes } from "react-router-dom";
-// import HomeTemplate from "../Templates/HomeTemplate/HomeTemplate.jsx";
-// import { CourseSingle } from "../component/CourseSingle/CourseSingle.jsx";
+import { useRoutes } from "react-router-dom";  
+import HomeTemplate from "../Templates/HomeTemplate/HomeTemplate.jsx";  
+import { CourseSingle } from "../component/CourseSingle/CourseSingle.jsx";  
+import CoursePage from "../component/CoursePage/CoursePage.jsx";  
+import LoginRegister from "../component/LoginRegister/LoginRegister.jsx";  
 
-// const useRouterCustome = () => {
-//   const router = useRoutes([
-//     {
-//       path: "/",
-//       element: <HomeTemplate />,
-//       children: [
-//         {
-//           index: true,
-//           element: <CourseSingle />,
-//         },
-//       ],
-//     },
-//   ]);
-//   return router;
-// };
-
-// export default useRouterCustome;
-
-
-
-import { useRoutes } from "react-router-dom";
-import HomeTemplate from "../Templates/HomeTemplate/HomeTemplate.jsx";
-import { CourseSingle } from "../component/CourseSingle/CourseSingle.jsx";
-import CoursePage from "../component/CoursePage/CoursePage.jsx";
-import LoginRegister from "../component/LoginRegister/LoginRegister.jsx";
-
-const useRouterCustome = () => {
-  const router = useRoutes([
-    {
-      path: "/", 
-      element: <HomeTemplate />,
-      children: [
-        {
-          index: true,
-          element: <CourseSingle />,
-        },
-      ],
-    },
-    {
-      path: "/courses", 
-      element: <HomeTemplate />, 
-      children: [
-        {
-          index: true,
-          element: <CoursePage />,
-        },
-      ],
-    },
-    {
-      path: "/loginregister", 
-      element: <HomeTemplate />, 
-      children: [
-        {
-          index: true,
-          element: <LoginRegister />,
-        },
-      ],
-    },
-  ]);
-  return router;
-};
+const useRouterCustome = () => {  
+  const router = useRoutes([  
+    {  
+      path: "/",   
+      element: <HomeTemplate />,  
+      children: [  
+        {  
+          index: true,  
+          element: <CoursePage />, // Changed from CourseSingle  
+        },  
+        {  
+          path: "course/:id", // Add this route for individual course details  
+          element: <CourseSingle />,  
+        },  
+      ],  
+    },  
+    {  
+      path: "/courses",   
+      element: <HomeTemplate />,   
+      children: [  
+        {  
+          index: true,  
+          element: <CoursePage />,  
+        },  
+        {  
+          path: ":id", // Dynamic route for specific course  
+          element: <CourseSingle />,  
+        },  
+      ],  
+    },  
+    {  
+      path: "/loginregister",   
+      element: <HomeTemplate />,   
+      children: [  
+        {  
+          index: true,  
+          element: <LoginRegister />,  
+        },  
+      ],  
+    },  
+  ]);  
+  return router;  
+};  
 
 export default useRouterCustome;
-
