b3482fefd5f084caacee9ae9119fc9d7b9253f98
MinhHungViolet <mhung11042004@gmail.com>
2024-11-23 22:21:03 +0700

    update add lecture, section trong trang xem video

--- frontend/src/hooks/useRouterCustome.jsx
+++ frontend/src/hooks/useRouterCustome.jsx
@@ -1,53 +1,58 @@
 // //import React from "react";
 
-import { useRoutes } from "react-router-dom";  
-import HomeTemplate from "../Templates/HomeTemplate/HomeTemplate.jsx";  
-import { CourseSingle } from "../component/CourseSingle/CourseSingle.jsx";  
-import CoursePage from "../component/CoursePage/CoursePage.jsx";  
-import LoginRegister from "../component/LoginRegister/LoginRegister.jsx";  
+import { useRoutes } from "react-router-dom";
+import HomeTemplate from "../Templates/HomeTemplate/HomeTemplate.jsx";
+import { CourseSingle } from "../component/CourseSingle/CourseSingle.jsx";
+import CoursePage from "../component/CoursePage/CoursePage.jsx";
+import LoginRegister from "../component/LoginRegister/LoginRegister.jsx";
+import LMSInterface from "../component/Light/Light.jsx";
 
-const useRouterCustome = () => {  
-  const router = useRoutes([  
-    {  
-      path: "/",   
-      element: <HomeTemplate />,  
-      children: [  
-        {  
-          index: true,  
-          element: <CoursePage />, // Changed from CourseSingle  
-        },  
-        {  
-          path: "course/:id", // Add this route for individual course details  
-          element: <CourseSingle />,  
-        },  
-      ],  
-    },  
-    {  
-      path: "/courses",   
-      element: <HomeTemplate />,   
-      children: [  
-        {  
-          index: true,  
-          element: <CoursePage />,  
-        },  
-        {  
-          path: ":id", // Dynamic route for specific course  
-          element: <CourseSingle />,  
-        },  
-      ],  
-    },  
-    {  
-      path: "/loginregister",   
-      element: <HomeTemplate />,   
-      children: [  
-        {  
-          index: true,  
-          element: <LoginRegister />,  
-        },  
-      ],  
-    },  
-  ]);  
-  return router;  
-};  
+const useRouterCustome = () => {
+  const router = useRoutes([
+    {
+      path: "/",
+      element: <HomeTemplate />,
+      children: [
+        {
+          index: true,
+          element: <CoursePage />, // Changed from CourseSingle
+        },
+        {
+          path: "course/:id", // Add this route for individual course details
+          element: <CourseSingle />,
+        },
+        {
+          path: "specific-lecture",
+          element: <LMSInterface />,
+        },
+      ],
+    },
+    {
+      path: "/courses",
+      element: <HomeTemplate />,
+      children: [
+        {
+          index: true,
+          element: <CoursePage />,
+        },
+        {
+          path: ":id", // Dynamic route for specific course
+          element: <CourseSingle />,
+        },
+      ],
+    },
+    {
+      path: "/loginregister",
+      element: <HomeTemplate />,
+      children: [
+        {
+          index: true,
+          element: <LoginRegister />,
+        },
+      ],
+    },
+  ]);
+  return router;
+};
 
 export default useRouterCustome;
